99202a7637f943dc2482d0421ec8e8e4806ccc57
Pratama Puji <pujiistiani123@gmail.com>
2023-05-05 20:14:52 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff8e3c",
+        secondary: "#d9376e",
+        dark: "#0d0d0d",
+        stroke: "#2a2a2a",
+        bg: "#eff0f3",
+      },
+    },
+  },
+  plugins: [],
+};
