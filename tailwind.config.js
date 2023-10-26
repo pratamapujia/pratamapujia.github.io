a30f658241fc5b5f35ee9a2a4d978ea5e1455e1e
pratamapujia <pujiistiani123@gmail.com>
2023-10-26 08:53:11 +0700

    new

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#ff5bd3',
+        secondary: '#5bd3ff',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
