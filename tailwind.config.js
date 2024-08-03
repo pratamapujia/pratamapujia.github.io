efb17cfdb24b3387f108963e375e58359f1f87d4
pratamapujia <pujiistiani123@gmail.com>
2024-08-03 11:08:06 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,20 @@ module.exports = {
         dark: '#0f172a',
       },
       screens: {
-        '2xl': '1320px',
+        'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
       },
     },
   },
